5a1a2399b0851ad4b9863df919c0f4f8edc31c36
emeraldhycient <igwezehycient86@gmail.com>
2023-08-23 23:25:10 +0100

    [add] api call

--- crypto/src/Components/Admin/Users.jsx
+++ crypto/src/Components/Admin/Users.jsx
@@ -1,65 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Users() {
-  return (
 
-      <div class="relative overflow-x-auto my-10">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                      <th scope="col" class="px-6 py-3">
-                        Name
-                      </th>
-                      <th scope="col" class="px-6 py-3">
-                          Email
-                      </th>
-                      <th scope="col" class="px-6 py-3">
-                          Profilepic
-                      </th>
+    const [usersFound, setusersFound] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:10000/api/users')
+            .then(res => {
+                setusersFound(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }, [])
+
+
+    return (
+        <div class="relative overflow-x-auto my-10">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Profilepic
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        usersFound?.users?.length > 0 ?
+                            usersFound?.users?.map((users) => (
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {users.name}
+                                </th>
+                                <td class="px-6 py-4">
+                                {users.email}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {users.profilepic}
+                                </td>
+                            </tr>
+                        
+                        ))
+                            : 
+                            <h1>Loading ...</h1>
+                    }
                     
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Apple MacBook Pro 17"
-                      </th>
-                      <td class="px-6 py-4">
-                          Silver
-                      </td>
-                      <td class="px-6 py-4">
-                          Laptop
-                      </td>
-                  </tr>
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Microsoft Surface Pro
-                      </th>
-                      <td class="px-6 py-4">
-                          White
-                      </td>
-                      <td class="px-6 py-4">
-                          Laptop PC
-                      </td>
-                  </tr>
-                  <tr class="bg-white dark:bg-gray-800">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Magic Mouse 2
-                      </th>
-                      <td class="px-6 py-4">
-                          Black
-                      </td>
-                      <td class="px-6 py-4">
-                          Accessories
-                      </td>
-                    
-                  </tr>
-              </tbody>
-          </table>
-      </div>
+                </tbody>
+            </table>
+        </div>
 
 
-  )
+    )
 }
 
 export default Users
