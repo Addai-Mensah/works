6d7b5e85ed3964e93f02a5388f3b17d80c5df3fb
emeraldhycient <igwezehycient86@gmail.com>
2023-08-27 22:35:29 +0100

    [update] changed initial state of the sidebar

--- crypto/src/Components/Dashboard/sidebar.jsx
+++ crypto/src/Components/Dashboard/sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 const Sidebar = () => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const Menus = [
     { title: "Dashboard", src: "Chart_fill" },
     { title: "Inbox", src: "Chat" },
